c18bc028fa7dd5af780eb56c6ff1b762069c3cae
Asad <asad.muhammad@tkxel.io>
2023-06-04 02:52:19 +0500

    php app

---
+++ index.php
@@ -0,0 +1,38 @@
+<?
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  $file = '/tmp/sample-app.log';
+  $message = file_get_contents('php://input');
+  file_put_contents($file, date('Y-m-d H:i:s') . " Received message: " . $message . "\n", FILE_APPEND);
+}
+else
+{
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Custom Application - AWS Elastic Beanstalk</title>
+    <meta name="viewport" content="width=device-width">
+
+    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="/styles.css" type="text/css">
+</head>
+<body>
+    <section class="leftSection">
+        <h1>WEEK 4 LAB</h1>
+<p>YOUR CUSTOM AWS ELASTIC BEANSTALK APPLICATION HAS BEEN DEPLOYED</p>
+    </section>
+
+    <section class="rightSection">
+
+<p></p>
+    </section>
+
+    <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
+</body>
+</html>
+<? 
+} 
+?>
